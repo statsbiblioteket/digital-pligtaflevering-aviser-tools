de2a3122d68744038be0db7e1db513989a012cfd
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2016-08-19 18:10:43 +0200

    for some reason classes were put in target/generated-sources - recovered and put in xxx

---
+++ tools/dpa-tools/src/main/java/xxx/VeraPdfMain.java
@@ -0,0 +1,28 @@
+package xxx;
+
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import dk.statsbiblioteket.digital_pligtaflevering_aviser.doms.DomsItem;
+import dk.statsbiblioteket.digital_pligtaflevering_aviser.harness.ConfigurationMap;
+import dk.statsbiblioteket.digital_pligtaflevering_aviser.harness.TaskComponent;
+
+import java.util.stream.Stream;
+
+/**
+ *
+ */
+public class VeraPdfMain {
+}
+
+@Component(modules = {ConfigurationMap.class, VeraPdfModule.class})
+interface VeraPdfTaskComponent extends TaskComponent<DomsItem, String> {
+};
+@Module
+class VeraPdfModule {
+    @Provides
+    Stream<Stream<DomsItem>> provideDomsItemStream() {
+        //return Stream.of(Stream.of("Item 1", "Item 2"));
+        return Stream.of(Stream.of());
+    }
+}
