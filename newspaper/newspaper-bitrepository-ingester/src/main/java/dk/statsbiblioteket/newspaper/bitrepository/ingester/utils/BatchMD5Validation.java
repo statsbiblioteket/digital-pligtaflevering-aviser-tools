9b7c9ce0f3373a53ad39d0aee791834537dd32fd
mmj <mmj@statsbiblioteket.dk>
2016-09-12 13:47:01 +0200

    After review keeping accepting undefined number of spaces between checksum and filename, and improving exception handling

--- newspaper/newspaper-bitrepository-ingester/src/main/java/dk/statsbiblioteket/newspaper/bitrepository/ingester/utils/BatchMD5Validation.java
+++ newspaper/newspaper-bitrepository-ingester/src/main/java/dk/statsbiblioteket/newspaper/bitrepository/ingester/utils/BatchMD5Validation.java
@@ -1,16 +1,21 @@
 package dk.statsbiblioteket.newspaper.bitrepository.ingester.utils;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Validator for md5 validation of digital newspaper batches
+ * Validator for md5 readChecksums of digital newspaper batches
  */
 public class BatchMD5Validation {
+    private static Logger log = LoggerFactory.getLogger(BatchMD5Validation.class);
     private String batchFolder;
     private Map<String, String> md5Map = new HashMap<String, String>();
 
@@ -22,8 +27,9 @@ public class BatchMD5Validation {
      * Validate a specified batch by reading the MD5SUMS, and confirm that all files in the folder has files with checksums matching the sṕecifications in the file
      * @param batchName The name of the batch where the checksum should be found
      */
-    public void validation(String batchName) {
-        try(BufferedReader br = new BufferedReader(new FileReader(this.batchFolder + File.separator+batchName + File.separator + "MD5SUMS.txt"))) {
+    public void readChecksums(String batchName) {
+        //try(BufferedReader br = new BufferedReader(new FileReader(this.batchFolder + File.separator+batchName + File.separator + "MD5SUMS.txt"))) {
+        try(BufferedReader br = new BufferedReader(new FileReader(Paths.get(this.batchFolder, batchName, "MD5SUMS.txt").toFile()))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -31,14 +37,14 @@ public class BatchMD5Validation {
                 //Example [8bd4797544edfba4f50c91c917a5fc81  verapdf/udgave1/pages/20160811-verapdf-udgave1-page001.pdf]
                 line = br.readLine();
                 if(line != null) {
-                    String[] split = line.split("  ");
+                    String[] split = line.split("\\s+");
                     String filename = batchName + File.separator + split[1];
                     String checksum = split[0];
                     md5Map.put(filename, checksum);
                 }
             }
         } catch(Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 
