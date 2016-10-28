096fdacb639df8c96c9262cbae8b6c2efd5cc01e
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2016-10-28 10:50:18 +0200

    Revised dpa-verapdf "does it work" test to ignore apparently random number in response, so test works.

--- main/dpa-verapdf/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/verapdf/VeraPDFValidatorTest.java
+++ main/dpa-verapdf/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/verapdf/VeraPDFValidatorTest.java
@@ -34,10 +34,17 @@ public class VeraPDFValidatorTest {
     @Test
     public void test6_8_2_2_t01_fail_a() {
         assertEquals(PDF1B_SUCCESS, validateResource1b("/veraPDF test suite 6-8-2-2-t01-fail-a.pdf"));
+        final String actualResponse = validateResource1a("/veraPDF test suite 6-8-2-2-t01-fail-a.pdf");
+        // For some reason the number in <code><assertion ordinal="..."</code> varies depending on context.
+        // For now, do a simple replacement on string level to get a passable test.
+
+        String fixedResponse = actualResponse.replaceFirst("<assertion ordinal=\"\\d+\"", "**DELETED**");
+
         assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
                 "<validationResult flavour=\"PDFA_1_A\" totalAssertions=\"254\" isCompliant=\"false\" xmlns=\"http://www.verapdf.org/ValidationProfile\">" +
                 "<assertions>" +
-                "<assertion ordinal=\"10\" status=\"FAILED\">" +
+                // "<assertion ordinal=\"10\" status=\"FAILED\">" +
+                "**DELETED** status=\"FAILED\">" +
                 "<ruleId specification=\"ISO_19005_1\" clause=\"6.8.2\" testNumber=\"1\"/>" +
                 "<message>The document catalog dictionary shall include a MarkInfo dictionary whose sole entry, Marked, shall have a value of true</message>" +
                 "<location>" +
@@ -46,7 +53,7 @@ public class VeraPDFValidatorTest {
                 "</location>" +
                 "</assertion>" +
                 "</assertions>" +
-                "</validationResult>", validateResource1a("/veraPDF test suite 6-8-2-2-t01-fail-a.pdf"));
+                "</validationResult>", fixedResponse);
     }
 
     private String validateResource1b(String resourceName) {
