a947bda7ffab9160f193265e6a684f354220c74a
mmj <mmj@statsbiblioteket.dk>
2016-11-21 13:13:33 +0100

    Starting to create a vagrant project for running autonomous components for dpa project

---
+++ tools/dpa-tools-deployment/vagrant/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+
+sudo mkdir dpa
+sudo chmod 777 dpa
+cd dpa
+
+
+
+sudo cp /target/dpa-tools-deployment-master-SNAPSHOT-package.tar.gz /home/vagrant/dpa/dpa-tools-deployment-master-SNAPSHOT-package.tar.gz
+tar -xf dpa-tools-deployment-master-SNAPSHOT-package.tar.gz
+
+
---
+++ tools/dpa-tools-deployment/vagrant/runningTheStuff.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./dpa-create-batch create-batch.properties createbatch.batchid=20160811 createbatch.roundtrip=1 autonomous.agent=register-batch-trigger
