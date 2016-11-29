c8ec6459cacdbad58e7d089bcc25784eefe22d92
mmj <mmj@statsbiblioteket.dk>
2016-11-29 15:26:30 +0100

    adding logenties that is supposed to be delivered to Kibana

---
+++ newspaper/newspaper-batch-trigger/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/newspaper/LoggingKeywords.java
@@ -0,0 +1,12 @@
+package dk.statsbiblioteket.medieplatform.autonomous.newspaper;
+
+/**
+ * Keywords of logs which is written to the logfile and used by logstash indicate progress in Kibana
+ */
+public class LoggingKeywords {
+
+
+    public static final String CREATE_BATCH_START_LOGTEXT = "EnteredMainMorDelivery dl_{}_rt{} ";
+    public static final String CREATE_BATCH_FINISH_LOGTEXT = "FinishedMainForDelivery dl_{}_rt{} at time: {} ms";
+
+    }
