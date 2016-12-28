f82e7605b34a3203bd4542fe6401ebc5a5a688f2
Thorbjoern Ravn Andersen <thorbjoern@gmail.com>
2016-12-28 14:44:09 +0100

    Changed to Jersey 1 to avoid conflict with bitrepository Jersey 1.17 client.

--- tools/dpa-tools-test/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/tryout/InitialJerseyServer.java
+++ tools/dpa-tools-test/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/tryout/InitialJerseyServer.java
@@ -1,9 +1,11 @@
 package dk.statsbiblioteket.digital_pligtaflevering_aviser.tools.tryout;
 
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
 
@@ -11,21 +13,37 @@ import java.net.URI;
  * Initial implementation stolen from Jersey quickstart Main
  */
 public class InitialJerseyServer {
+    private static int getPort(int defaultPort) {
+        //grab port from environment, otherwise fall back to default port 9998
+        String httpPort = System.getProperty("jersey.test.port");
+        if (null != httpPort) {
+            try {
+                return Integer.parseInt(httpPort);
+            } catch (NumberFormatException e) {
+            }
+        }
+        return defaultPort;
+    }
+    private static URI getBaseURI() {
+        return UriBuilder.fromUri("http://localhost/").port(getPort(9998)).build();
+    }
+
+    public static final URI BASE_URI = getBaseURI();
+
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
-    public static HttpServer startServer() {
+    public static HttpServer startServer() throws IOException {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.example package
-        final ResourceConfig rc = new ResourceConfig().packages("dk.statsbiblioteket");
+        ResourceConfig resourceConfig = new PackagesResourceConfig("dk.statsbiblioteket.digital_pligtaflevering_aviser.tools.tryout");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyServerFactory.createHttpServer(BASE_URI, resourceConfig);
     }
 
     /**
