3e090986f772c1228533d3b2b7aea4a45df7e332
mmj <mmj@statsbiblioteket.dk>
2016-12-29 13:50:25 +0100

    DPA-64: Chaning xsd to the versions suggested to infomedia

--- tools/dpa-tools-test/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/main/ValidateXMLMainTest.java
+++ tools/dpa-tools-test/src/test/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/main/ValidateXMLMainTest.java
@@ -1,14 +1,6 @@
 package dk.statsbiblioteket.digital_pligtaflevering_aviser.tools.main;
 
-import org.xml.sax.InputSource;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Map;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Developer class for stating the class ValidateXMLMain
@@ -17,56 +9,6 @@ import static org.junit.Assert.assertEquals;
 public class ValidateXMLMainTest {
 
 
-
-    @org.junit.Test
-    public void analyzeAcceptedXMLArticleTest() throws Exception {
-
-        URL xmlurl = getClass().getClassLoader().getResource("testResources/articleTest.xml");
-        ValidateXMLMain.ValidateXMLModule xmlValidatorModule = new ValidateXMLMain.ValidateXMLModule();
-        String xsdName = xmlValidatorModule.getRootName( new InputSource(xmlurl.getFile()));
-        Map<String, String> xsdMap = xmlValidatorModule.provideXsdRootMap();
-        URL xsdUrl = getClass().getClassLoader().getResource(xsdMap.get(xsdName));
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(xmlurl.getFile()), "UTF8"));
-        assertEquals("Test of approved article", true, xmlValidatorModule.tryParsing(in, xsdUrl));
-    }
-
-
-    @org.junit.Test
-    public void analyzeAcceptedXMLPageTest() throws Exception {
-
-        URL xmlurl = getClass().getClassLoader().getResource("testResources/pageTest.xml");
-        ValidateXMLMain.ValidateXMLModule xmlValidatorModule = new ValidateXMLMain.ValidateXMLModule();
-        String xsdName = xmlValidatorModule.getRootName( new InputSource(xmlurl.getFile()));
-        Map<String, String> xsdMap = xmlValidatorModule.provideXsdRootMap();
-        URL xsdUrl = getClass().getClassLoader().getResource(xsdMap.get(xsdName));
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(xmlurl.getFile()), "UTF8"));
-        assertEquals("Test of approved page", true, xmlValidatorModule.tryParsing(in, xsdUrl));
-    }
-
-    @org.junit.Test
-    public void analyzeFailingXMLArticleTest() throws Exception {
-
-        URL xmlurl = getClass().getClassLoader().getResource("testResources/articleFailTest.xml");
-        ValidateXMLMain.ValidateXMLModule xmlValidatorModule = new ValidateXMLMain.ValidateXMLModule();
-        String xsdName = xmlValidatorModule.getRootName( new InputSource(xmlurl.getFile()));
-        Map<String, String> xsdMap = xmlValidatorModule.provideXsdRootMap();
-        URL xsdUrl = getClass().getClassLoader().getResource(xsdMap.get(xsdName));
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(xmlurl.getFile()), "UTF8"));
-        assertEquals("Test of failing article", false, xmlValidatorModule.tryParsing(in, xsdUrl));
-    }
-
-
-    @org.junit.Test
-    public void analyzeFailingXMLPageTest() throws Exception {
-
-        URL xmlurl = getClass().getClassLoader().getResource("testResources/pageFailTest.xml");
-        ValidateXMLMain.ValidateXMLModule xmlValidatorModule = new ValidateXMLMain.ValidateXMLModule();
-        String xsdName = xmlValidatorModule.getRootName( new InputSource(xmlurl.getFile()));
-        Map<String, String> xsdMap = xmlValidatorModule.provideXsdRootMap();
-        URL xsdUrl = getClass().getClassLoader().getResource(xsdMap.get(xsdName));
-        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(xmlurl.getFile()), "UTF8"));
-        assertEquals("Test of failing page", false, xmlValidatorModule.tryParsing(in, xsdUrl));
-    }
 
     @org.junit.Test
     public void invocationTest() throws URISyntaxException {
