18c8330762cce3b2d4cc458abc3c0f32a5a52861
tra <tra@kb.dk>
2017-03-17 15:14:12 +0100

    DPA-99:  Now doing double urlencoding on indvidual path names to get the expected result.

--- tools/dpa-tools/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/modules/BitRepositoryModule.java
+++ tools/dpa-tools/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/modules/BitRepositoryModule.java
@@ -3,11 +3,13 @@ package dk.statsbiblioteket.digital_pligtaflevering_aviser.tools.modules;
 import dagger.Module;
 import dagger.Provides;
 import dk.statsbiblioteket.digital_pligtaflevering_aviser.harness.ConfigurationMap;
-import org.apache.commons.codec.CharEncoding;
 
 import javax.inject.Named;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.StringJoiner;
 import java.util.function.Function;
 
 import static dk.statsbiblioteket.medieplatform.autonomous.iterator.bitrepository.IngesterConfiguration.BITMAG_BASEURL_PROPERTY;
@@ -21,7 +23,8 @@ public class BitRepositoryModule {
     public static final String BITREPOSITORY_SBPILLAR_MOUNTPOINT = "bitrepository.sbpillar.mountpoint";
     public static final String PROVIDE_ENCODE_PUBLIC_URL_FOR_FILEID = "provide.encode.public.url.for.fileid";
 
-    /** This is the base url for the public exposure of a given file ingested in the BitRepository.  This
+    /**
+     * This is the base url for the public exposure of a given file ingested in the BitRepository.  This
      * is stored as the URL of the CONTENTS datastream.
      * For the SB pillar we can remove this prefix from the URL and get the actual relative filename on disk.
      * Typically this is stored
@@ -30,11 +33,14 @@ public class BitRepositoryModule {
      * @return the configuration parameter provided (required)
      */
     @Provides
-    public @Named(BITMAG_BASEURL_PROPERTY) String provideBitrepositoryBaseUrl(ConfigurationMap map) {
+    public
+    @Named(BITMAG_BASEURL_PROPERTY)
+    String provideBitrepositoryBaseUrl(ConfigurationMap map) {
         return map.getRequired(BITMAG_BASEURL_PROPERTY);
     }
 
-    /** The SB Bitrepository pillar allows direct access to the files in the repository, typically
+    /**
+     * The SB Bitrepository pillar allows direct access to the files in the repository, typically
      * in the form of a NFS mount point from where the file path can be resolved.
      *
      * @param map configuration map
@@ -46,19 +52,25 @@ public class BitRepositoryModule {
         return map.getRequired(BITREPOSITORY_SBPILLAR_MOUNTPOINT);
     }
 
-
     @Provides
     @Named(PROVIDE_ENCODE_PUBLIC_URL_FOR_FILEID)
     public Function<String, String> provideEncodePublicURLForFileID(@Named(BITMAG_BASEURL_PROPERTY) String bitmagUrl) {
+
         return fileID -> {
-            String singleEncodedFileId = null;
-            try {
-                singleEncodedFileId = URLEncoder.encode(fileID, CharEncoding.UTF_8);
-                String doubleEncodedFileId = URLEncoder.encode(singleEncodedFileId, CharEncoding.UTF_8);
-                return bitmagUrl + doubleEncodedFileId;
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
+            Path path = Paths.get(fileID);
+
+            StringBuilder sb = new StringBuilder(bitmagUrl);
+
+            StringJoiner sj = new StringJoiner("/");
+            for (Path name : path) {
+                try {
+                    // We must encode _twice_ to get #'s right in the DOMS external link.
+                    sj.add(URLEncoder.encode(URLEncoder.encode(name.toString(), "UTF-8"), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
+            return bitmagUrl + sj;
         };
     }
 }
