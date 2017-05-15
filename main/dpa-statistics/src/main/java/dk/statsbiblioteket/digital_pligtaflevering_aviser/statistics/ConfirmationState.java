2c30c3fb891fb53adfe59371d2d6274515022257
mmj <mmj@statsbiblioteket.dk>
2017-05-15 09:09:41 +0200

    DPA-67: Documentation to enumerations

--- main/dpa-statistics/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/statistics/ConfirmationState.java
+++ main/dpa-statistics/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/statistics/ConfirmationState.java
@@ -1,8 +1,10 @@
 package dk.statsbiblioteket.digital_pligtaflevering_aviser.statistics;
 
 /**
- * Created by mmj on 4/11/17.
+ * Enums to indicate different validation states for items in a newspaperdelivery
  */
 public enum ConfirmationState {
-    UNCHECKED, CHECKED, REJECTED;
+    UNCHECKED, // The value before the user has performed a check of a page or article
+    CHECKED, // The value when the user has performed a check of a page or article
+    REJECTED; // The value when the user has indicated that there is something wrong with a page or article
 }
