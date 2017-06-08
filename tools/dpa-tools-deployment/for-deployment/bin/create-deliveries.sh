0c1fb6318d7a383b02e7dd39364a79b886390279
tra <tra@kb.dk>
2017-06-08 18:22:14 +0200

    DPA-35:  Stage-feedback from JHLJ.  ITERATOR_FILESYSTEM_BATCHES_FOLDER may point to single delivery.

--- tools/dpa-tools-deployment/for-deployment/bin/create-deliveries.sh
+++ tools/dpa-tools-deployment/for-deployment/bin/create-deliveries.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd $HOME/logs
-flock -n ~/var/locks/dpa-create-deliveries $HOME/services/java-wrappers/dpa-create-delivery $HOME/services/conf/create-deliveries.properties iterator.filesystem.batches.folder=$HOME/deliveries "$@"
+flock -n ~/var/locks/dpa-create-deliveries $HOME/services/java-wrappers/dpa-create-delivery $HOME/services/conf/create-deliveries.properties "$@"
