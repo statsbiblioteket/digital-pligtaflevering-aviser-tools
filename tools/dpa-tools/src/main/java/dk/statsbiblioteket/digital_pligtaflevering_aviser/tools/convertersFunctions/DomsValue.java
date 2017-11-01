580989b8997b7822c83696c21cb6fb5e2d64ef87
Thorbjoern Ravn Andersen <thorbjoern@gmail.com>
2017-11-01 23:53:59 +0100

    ContextValue -> StreamIdValue - plus test case.

--- tools/dpa-tools/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/convertersFunctions/DomsValue.java
+++ tools/dpa-tools/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/tools/convertersFunctions/DomsValue.java
@@ -5,8 +5,8 @@ import dk.statsbiblioteket.digital_pligtaflevering_aviser.doms.DomsId;
 /**
  *
  */
-public class DomsValue<V> extends ContextValue<DomsId, V> {
-    public DomsValue(DomsId context, V value) {
-        super(context, value);
+public class DomsValue<V> extends StreamIdValue<DomsId, V> {
+    public DomsValue(DomsId id, V value) {
+        super(id, value);
     }
 }
