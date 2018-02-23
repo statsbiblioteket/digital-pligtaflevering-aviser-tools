dbc9bce395577866f39ca7195ae7ff2f1ea1b0f0
tra <tra@kb.dk>
2018-02-23 15:01:39 +0100

    StateIngested now shows newest first.

--- tools/dpa-dashboard-cdi/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/dashboard/StateIngested.java
+++ tools/dpa-dashboard-cdi/src/main/java/dk/statsbiblioteket/digital_pligtaflevering_aviser/dashboard/StateIngested.java
@@ -9,9 +9,7 @@ import dk.statsbiblioteket.digital_pligtaflevering_aviser.doms.DomsItem;
 import dk.statsbiblioteket.digital_pligtaflevering_aviser.doms.DomsRepository;
 import dk.statsbiblioteket.digital_pligtaflevering_aviser.doms.SBOIQuerySpecification;
 import dk.statsbiblioteket.digital_pligtaflevering_aviser.harness.ConfigurationMap;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletException;
@@ -19,9 +17,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- *
  * @author tra
  */
 @WebServlet(name = "StateIngested", urlPatterns = {"/StateIngested"})
@@ -38,10 +39,10 @@ public class StateIngested extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -58,6 +59,8 @@ public class StateIngested extends HttpServlet {
         List<DomsItem> l = repository.query(new SBOIQuerySpecification(SBOIConstants.Q_INGEST_SUCCESSFUL))
                 .collect(Collectors.toList());
 
+        l.sort(Comparator.comparing(DomsItem::getPath).reversed());
+
         request.setAttribute("l", l);
         request.setAttribute("h", "Successful Ingest " + domsUsername);
 
@@ -65,13 +68,14 @@ public class StateIngested extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -82,10 +86,10 @@ public class StateIngested extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
