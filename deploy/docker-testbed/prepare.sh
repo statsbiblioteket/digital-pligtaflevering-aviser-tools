14a800617da086aa6afb7be9ad0f4ed54471136b
tra <tra@kb.dk>
2018-06-18 15:43:57 +0200

    DPA-147: dpa-cron instance now executes autonomous components.

--- deploy/docker-testbed/prepare.sh
+++ deploy/docker-testbed/prepare.sh
@@ -2,13 +2,13 @@
 
 set -e
 
+# using target/ similar to Maven.
+rm -rf target/
+
 # Download and unpack deployment tarball
 mvn -q org.apache.maven.plugins:maven-dependency-plugin:3.1.0:unpack \
   -Dartifact=dk.statsbiblioteket.digital_pligtaflevering_aviser.tools:dpa-tools-deployment:master-SNAPSHOT:tar.gz \
-  -Dproject.basedir=. -DoutputDirectory=target/for-docker -Dsilent=true
+  -Dproject.basedir=. -DoutputDirectory=target/for-docker -Dsilent=true \
+&& du -sk target/for-docker
 
-# Create crontab file here.
 
-# Build container
-
-docker build -t dpa-cron .
